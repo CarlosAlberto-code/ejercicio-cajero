72b8c83c41a86eb607e8d8e4fe13ab638951d96a
Carlos Alberto Bolanos Hernandez <carlosalberto.bolanos.hernand.contractor@bbva.com>
2022-10-10 14:47:51 -0500

    chore: creacion de la clase menu

---
+++ Cajero python/Menu.py
@@ -0,0 +1,23 @@
+class Menu:
+
+    def mostrar(self):
+        print("Selecciona una opcion")
+        print("1.- Ver saldo actual")
+        print("2.- Ver historico de movimientos")
+        print("3.- Retirar saldo")
+        print("4.- Salir")
+        return self.validadorDeEntrada()
+
+    def validadorDeEntrada(self):
+        error = True
+        while error:
+            entrada = raw_input("Selecciona una opcion: ")
+            if entrada.isdigit():
+                if int(entrada) <= 4 and int(entrada) >= 1:
+                    error = False
+                else:
+                    print("\n Opcion no valida, ingresa un valor entre 1 y 4.")
+            else:
+                print("\n Opcion no valida, ingresa de nuevo")
+        
+        return entrada
